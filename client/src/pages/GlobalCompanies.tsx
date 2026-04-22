da43d68ae1384f1ca710a02420e146b7a70186eb
shekharzorin <shekharzorin@gmail.com>
2026-04-22 16:21:02 +0530

    Refactor handleSubmit to improve readabilityfix: remove unused res variable in GlobalCompanies.tsx

--- client/src/pages/GlobalCompanies.tsx
+++ client/src/pages/GlobalCompanies.tsx
@@ -43,7 +43,7 @@ export const GlobalCompanies: React.FC = () => {
         e.preventDefault();
         setSubmitting(true);
         try {
-            let res;
+            
             if (editingId) {
                 await api.put(`/companies/${editingId}`, formData);
             } else {
